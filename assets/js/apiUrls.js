ebee114052e4962128564cb7da2b3ffd72435342
anhtuana2k422001 <anhtuana2k422001@gmail.com>
2023-10-08 21:30:06 +0700

    create app csdl suport ui

---
+++ assets/js/apiUrls.js
@@ -0,0 +1,4 @@
+export const API_LIST_CSDL = 'https://csdl.hoanhtuan.online/api/v1/danh-sach-luoc-do-mau';
+export const API_PHAN_TICH = 'https://csdl.hoanhtuan.online/api/v1/phan-tich-csdl';
+export const API_TIM_BAO_DONG = 'https://csdl.hoanhtuan.online/api/v1/tim-bao-dong';
+
