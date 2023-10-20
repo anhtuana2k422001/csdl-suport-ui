5f5cb2a86f311a0a27ecd7289f9eccbb4f017455
anhtuana2k422001 <anhtuana2k422001@gmail.com>
2023-10-21 01:10:00 +0700

    handle code js to res for html display'

--- content/js/main.js
+++ content/js/main.js
@@ -1,190 +1,211 @@
-import { API_PHAN_TICH } from './config/ApiUrls.js';
-import { CreateUUID, EMPTY} from './utils/Utilities.js';
-import { getListCSDL, phanTichCSDL} from './api/ApiCSDL.js';
+import { API_PHAN_TICH } from "./config/ApiUrls.js";
+import { CreateUUID, EMPTY } from "./utils/Utilities.js";
+import { getListCSDL, phanTichCSDL } from "./api/ApiCSDL.js";
 // Sử dụng apiListCSDL và apiPhanTich
 
 async function start() {
-    // handListCSDL(API_LIST_CSDL);
-    handlePhanTichCSDL(API_PHAN_TICH);
+  // handListCSDL(API_LIST_CSDL);
+  handlePhanTichCSDL(API_PHAN_TICH);
 }
 
- start();
+start();
 
-// Render 
- function renderListCSDL(response) {
-    var listCSDlExample = document.getElementById('csdl-block');
-    console.log(response.resultCode);
-    if(response.resultCode === '000'){
-        let data = response.data;
-        var html = data.map(function(item) {
-            return `<div>
+// Render
+function renderListCSDL(response) {
+  var listCSDlExample = document.getElementById("csdl-block");
+  console.log(response.resultCode);
+  if (response.resultCode === "000") {
+    let data = response.data;
+    var html = data.map(function (item) {
+      return `<div>
                 <h2 class='title-name'>${item.name}</h2>      
                 <p>Tập thuộc tính: ${item.attributeSet}</p>     
                 <span>Tập phụ thuộc hàm: ${item.dependencyChain}</span>       
             </div>`;
-        });
+    });
 
-        var content = html.join('');
-        listCSDlExample.innerHTML = content;
-       
-    }else{
-        alert("Có lỗi: ");
-    }
- }
+    var content = html.join("");
+    listCSDlExample.innerHTML = content;
+  } else {
+    alert("Có lỗi: ");
+  }
+}
 
- function renderhanTichCSDL(response) {
+function renderhanTichCSDL(response) {
+  if (response.resultCode === "000") {
+    var hrElement = document.createElement("hr");
+    const informationList = document.querySelector(".information-list");
 
-    if(response.resultCode === '000'){
-        const informationList = document.querySelector('.information-list');
+    const titleInformationList = document.createElement("legend");
+    titleInformationList.classList.add(
+      "scheduler-border",
+      "mb-0",
+      "font-weight-bold",
+      "text-uppercase"
+    );
+    titleInformationList.textContent = "THÔNG TIN LƯỢC ĐỒ QUAN HỆ";
 
-        const titleInformationList = document.createElement('legend');
-        titleInformationList.classList.add('scheduler-border', 'mb-0', 'font-weight-bold', 'text-uppercase')
-        titleInformationList.textContent = "THÔNG TIN LƯỢC ĐỒ QUAN HỆ";
+    informationList.appendChild(titleInformationList);
 
-        informationList.appendChild(titleInformationList);
-        
-        
-        const listItem = document.createElement('div');
-        const information = response.data.information.dataInfo; 
-        const html = information.map(function(item) {
-            return `<p>${item.description}: <b> ${item.info}</b></p> <hr />`
-        });
+    const listItem = document.createElement("div");
+    const information = response.data.information.dataInfo;
+    const html = information.map(function (item) {
+      return `<p>${item.description}: <b> ${item.info}</b></p> <hr />`;
+    });
 
-        listItem.innerHTML = html.join('');
+    listItem.innerHTML = html.join("");
 
-        informationList.appendChild(listItem);
+    informationList.appendChild(listItem);
 
-        // // Step 1: Tìm khóa chính
-        // const primaryKeyContainer = document.getElementById('primaryKey');
-        // const primaryKeyData = response.data.primaryKey;
+    // Step 1: Tìm khóa chính
+    const primaryKeyContainer = document.getElementById("primaryKey");
+    const primaryKeyData = response.data.primaryKey;
 
-        // const titlePrimary = document.createElement('h2');
-        // titlePrimary.textContent = primaryKeyData.title;
-        
-        // const valueListPrimary = document.createElement('ul');
-        // primaryKeyData.value.forEach((stepData) => {
-        //     const stepElement = document.createElement('li');
-        //     const stepTitleElement = document.createElement('p');
-        //     stepTitleElement.textContent = stepData.step;
+    const titlePrimary = document.createElement("p");
+    titlePrimary.classList.add(
+      "indigo-text",
+      "font-weight-bold",
+      "text-uppercase"
+    );
+    titlePrimary.textContent = primaryKeyData.title;
 
-        //     const stepTextListElement = document.createElement('ul');
-        //     stepData.text.forEach((text) => {
-        //         const stepTextElement = document.createElement('li');
-        //         stepTextElement.textContent = text;
-        //         stepTextListElement.appendChild(stepTextElement);
-        //     });
+    const valueListPrimary = document.createElement("div");
+    primaryKeyData.value.forEach((stepData) => {
+      const stepElement = document.createElement("b");
+      stepElement.textContent = stepData.step;
 
-        //     stepElement.appendChild(stepTitleElement);
-        //     stepElement.appendChild(stepTextListElement);
-        //     valueListPrimary.appendChild(stepElement);
-        // });
+      const stepTextListElement = document.createElement("ul");
+      stepData.text.forEach((text) => {
+        const stepTextElement = document.createElement("li");
+        stepTextElement.textContent = text;
+        stepTextElement.classList.add("text-content");
+        stepTextListElement.appendChild(stepTextElement);
+      });
 
-        // const resultPrimary = document.createElement('p');
-        // resultPrimary.textContent = "Kết luận: " + primaryKeyData.result;
+      valueListPrimary.appendChild(stepElement);
+      valueListPrimary.appendChild(stepTextListElement);
+    });
 
-        // // Thêm các phần tử HTML vào container
-        // primaryKeyContainer.appendChild(titlePrimary);
-        // primaryKeyContainer.appendChild(valueListPrimary);
-        // primaryKeyContainer.appendChild(resultPrimary);
+    const resultPrimary = document.createElement("b");
+    resultPrimary.textContent = primaryKeyData.result;
 
-        // // Step 2: Tìm phụ thuộc hàm
-        // const minimalCoveContainer = document.getElementById('minimalCove-container');
-        // const minimalCoveData = response.data.minimalCove;
+    // // Thêm các phần tử HTML vào container
+    primaryKeyContainer.appendChild(titlePrimary);
 
-        // const titleElement = document.createElement('h2');
-        // titleElement.textContent = minimalCoveData.title;
+    primaryKeyContainer.appendChild(hrElement);
+    primaryKeyContainer.appendChild(valueListPrimary);
+    primaryKeyContainer.appendChild(resultPrimary);
 
-        // const valueListElement = document.createElement('ul');
-        // minimalCoveData.value.forEach((stepData) => {
-        //     const stepElement = document.createElement('li');
-        //     const stepTitleElement = document.createElement('p');
-        //     stepTitleElement.textContent = stepData.step;
+    // Step 2: Tìm phụ thuộc hàm
+    const minimalCoveContainer = document.getElementById(
+      "minimalCove-container"
+    );
+    const minimalCoveData = response.data.minimalCove;
 
-        //     const stepTextListElement = document.createElement('ul');
-        //     stepData.text.forEach((text) => {
-        //         const stepTextElement = document.createElement('li');
-        //         stepTextElement.textContent = text;
-        //         stepTextListElement.appendChild(stepTextElement);
-        //     });
+    const titleMinimalCove = document.createElement("p");
+    titleMinimalCove.classList.add(
+      "indigo-text",
+      "font-weight-bold",
+      "text-uppercase"
+    );
+    titleMinimalCove.textContent = minimalCoveData.title;
 
-        //     stepElement.appendChild(stepTitleElement);
-        //     stepElement.appendChild(stepTextListElement);
-        //     valueListElement.appendChild(stepElement);
-        // });
+    const valueListMinimalCove = document.createElement("div");
+    minimalCoveData.value.forEach((stepData) => {
+      const stepElement = document.createElement("b");
+      stepElement.textContent = stepData.step;
 
-        // const resultElement = document.createElement('p');
-        // resultElement.textContent = minimalCoveData.result;
+      const stepTextListElement = document.createElement("ul");
+      stepData.text.forEach((text) => {
+        const stepTextElement = document.createElement("li");
+        stepTextElement.textContent = text;
+        stepTextElement.classList.add("text-content");
+        stepTextListElement.appendChild(stepTextElement);
+      });
 
-        //   // Thêm các phần tử HTML vào container
-        //   minimalCoveContainer.appendChild(titleElement);
-        //   minimalCoveContainer.appendChild(valueListElement);
-        //   minimalCoveContainer.appendChild(resultElement);
+      valueListMinimalCove.appendChild(stepElement);
+      valueListMinimalCove.appendChild(stepTextListElement);
+    });
 
+    const resultMinimalCove = document.createElement("b");
+    resultMinimalCove.textContent = minimalCoveData.result;
 
-        // // Step 3: Tìm dạng chuẩn
-        // const normalFormContainer = document.getElementById('normalForm');
-        // const normalFormData = response.data.normalForm;
-        // const titleNormalForm = document.createElement('h2');
-        // titleNormalForm.textContent = normalFormData.title;
+    // // Thêm các phần tử HTML vào container
+    minimalCoveContainer.appendChild(titleMinimalCove);
+    minimalCoveContainer.appendChild(valueListMinimalCove);
+    minimalCoveContainer.appendChild(resultMinimalCove);
 
-        // const valueListNormalForm = document.createElement('ul');
-        // normalFormData.value.forEach((stepData) => {
-        //     const stepElement = document.createElement('li');
-        //     const stepTitleElement = document.createElement('p');
-        //     stepTitleElement.textContent = stepData.step;
+    // Step 3: Tìm dạng chuẩn
+    const normalFormContainer = document.getElementById("normalForm");
+    const normalFormData = response.data.normalForm;
 
-        //     const stepTextListElement = document.createElement('ul');
-        //     stepData.text.forEach((text) => {
-        //         const stepTextElement = document.createElement('li');
-        //         stepTextElement.textContent = text;
-        //         stepTextListElement.appendChild(stepTextElement);
-        //     });
+    const titleNormalForm = document.createElement("p");
+    titleNormalForm.classList.add(
+      "indigo-text",
+      "font-weight-bold",
+      "text-uppercase"
+    );
+    titleNormalForm.textContent = normalFormData.title;
 
-        //     stepElement.appendChild(stepTitleElement);
-        //     stepElement.appendChild(stepTextListElement);
-        //     valueListNormalForm.appendChild(stepElement);
-        // });
+    const valueListNormalForm = document.createElement("div");
+    normalFormData.value.forEach((stepData) => {
+      const stepElement = document.createElement("b");
+      stepElement.textContent = stepData.step;
 
-        // const resultNormalForm = document.createElement('p');
-        // resultNormalForm.textContent = normalFormData.result;
+      const stepTextListElement = document.createElement("ul");
+      stepData.text.forEach((text) => {
+        const stepTextElement = document.createElement("li");
+        stepTextElement.textContent = text;
+        stepTextElement.classList.add("text-content");
+        stepTextListElement.appendChild(stepTextElement);
+      });
 
-        // // Thêm các phần tử HTML vào container
-        // normalFormContainer.appendChild(titleNormalForm);
-        // normalFormContainer.appendChild(valueListNormalForm);
-        // normalFormContainer.appendChild(resultNormalForm);
+      valueListNormalForm.appendChild(stepElement);
+      valueListNormalForm.appendChild(stepTextListElement);
+    });
 
+    const resultNormalForm = document.createElement("b");
+    resultNormalForm.textContent = normalFormData.result;
 
-    }else{
-        var resultMessage = document.querySelector('.message-error');
-        resultMessage.innerHTML = response.resultMessage
-    }
- }
+    // // Thêm các phần tử HTML vào container
+    normalFormContainer.appendChild(titleNormalForm);
+    normalFormContainer.appendChild(valueListNormalForm);
+    normalFormContainer.appendChild(resultNormalForm);
+  } else {
+    var resultMessage = document.querySelector(".message-error");
+    resultMessage.innerHTML = response.resultMessage;
+  }
+}
 
 async function handListCSDL(apiCall) {
-    renderListCSDL(await getListCSDL(apiCall));
+  renderListCSDL(await getListCSDL(apiCall));
 }
 
- function handlePhanTichCSDL(apiCall){
-    var check = document.getElementById('button-start');
-    check.onclick = async function(){
-        document.querySelector('.information-list').innerHTML = EMPTY;
-        // document.getElementById('primaryKey').innerHTML = EMPTY;
-        // document.getElementById('minimalCove-container').innerHTML = EMPTY;
-        // document.getElementById('normalForm').innerHTML = EMPTY;
-        // // document.querySelector('.message-error').innerHTML = EMPTY
-        
-        var attributeSet = document.querySelector('input[name="attributeSet"]').value;
-        var dependencyChain = document.querySelector('input[name="dependencyChain"]').value;
+function handlePhanTichCSDL(apiCall) {
+  var check = document.getElementById("button-start");
+  check.onclick = async function () {
+    document.querySelector(".information-list").innerHTML = EMPTY;
+    document.getElementById("primaryKey").innerHTML = EMPTY;
+    document.getElementById("minimalCove-container").innerHTML = EMPTY;
+    document.getElementById("normalForm").innerHTML = EMPTY;
+    document.querySelector(".message-error").innerHTML = EMPTY;
+    document.querySelector(".card.z-depth-1").style.display = "block";
 
-        var formData = {
-            requestId: CreateUUID(),
-            attributeSet: attributeSet,
-            dependencyChain: dependencyChain
-        }
+    var attributeSet = document.querySelector(
+      'input[name="attributeSet"]'
+    ).value;
+    var dependencyChain = document.querySelector(
+      'input[name="dependencyChain"]'
+    ).value;
 
-        console.log(formData);
-        
-        renderhanTichCSDL(await phanTichCSDL(formData, apiCall))
+    var formData = {
+      requestId: CreateUUID(),
+      attributeSet: attributeSet,
+      dependencyChain: dependencyChain,
     };
-}
 
+    console.log(formData);
+
+    renderhanTichCSDL(await phanTichCSDL(formData, apiCall));
+  };
+}
